d99360cbb6555d494829705414d2982fa01ba805
Maxim Rozanov <m.rozanoov@gmail.com>
2024-12-06 19:55:04 +0300

    Исправления 2

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -42,7 +42,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthorOrAdminOrReadOnly, ]
     pagination_class = CustomPagination
-    queryset = Recipe.objects.all().order_by('-pub_date')
+    queryset = Recipe.objects.all()
     filter_backends = [DjangoFilterBackend, ]
     filterset_class = RecipeFilter
 

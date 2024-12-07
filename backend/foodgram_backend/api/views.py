d58d69d0633b87765333e21f25f3338680853f84
Maxim Rozanov <m.rozanoov@gmail.com>
2024-12-07 23:15:49 +0300

    Пллииииииз работаааай

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action, api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
